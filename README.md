51fa3bca84913c03e5f1bd7de9c5937d0f6c5d4d
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-02-27 15:54:51 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# X-DE Loot
+
+Adds a loot chest, a command to prevent spawning loot in the chest so you can save a schematic with a loot chest:
+/loot enable
+/loot disable
+
+Also adds 2 functions to create loot in other chests and/or with a different dungeon type (dungeon type is optional):
+add_loot("name", "description", "mod:chest", "dungeontype")
+add_loot_adv("mod:node", "mod:chest", "dungeontype")
+
+You can add a new item to the loot pool with this function:
+dungeon_loot.register({name = "default:dirt", chance = 0.6, count = {2, 16}, y = {-64, 32768}, types = {"normal", "sandstone", "desert"}})
+
+If you want to create a new dungeon type only in types = {" "} write the new dungeontype
+
+Here are all the dungeon types that are already created:
+"normal", "sandstone", "desert", "ice"
+
+If you don't put count, the item in the loot will always be 1, if you don't put y, it will appear on any coordinate, if you don't put types, it will appear in all loot chests
+
+## Installation
+
+### ContentDB
+
+* Content > Browse Online Content
+* Search for "X-DE Loot"
+* Click Install
+
+### Manually
+
+- Unzip the archive, rename the folder to `xde_loot` and
+place it in `.../minetest/mods/`
+
+- GNU/Linux: If you use a system-wide installation place it in `~/.minetest/mods/`.
+
+The Luanti engine can be found at [GitHub](https://github.com/minetest/minetest).
+
+For further information or help, see: [Installing Mods](https://wiki.luanti.org/Installing_Mods).
+
+## License
+
+See `LICENSE.txt`
---
+++ init.lua
@@ -0,0 +1,155 @@
+
+loot = true
+
+if minetest.get_modpath("dungeon_loot") then
+
+	local function noise3d_integer(noise, pos)
+		return math.abs(math.floor(noise:get_3d(pos) * 0x7fffffff))
+	end
+
+	local function random_sample(rand, list, count)
+		local ret = {}
+		for n = 1, count do
+			local idx = rand:next(1, #list)
+			table.insert(ret, list[idx])
+			table.remove(list, idx)
+		end
+		return ret
+	end
+
+	local function populate_chest(pos, rand, dungeontype)
+		--minetest.chat_send_all("chest placed at " .. minetest.pos_to_string(pos) .. " [" .. dungeontype .. "]")
+		--minetest.add_node(vector.add(pos, {x=0, y=1, z=0}), {name="default:torch", param2=1})
+
+		local item_list = dungeon_loot._internal_get_loot(pos.y, dungeontype)
+		-- take random (partial) sample of all possible items
+		local sample_n = math.min(#item_list, dungeon_loot.STACKS_PER_CHEST_MAX)
+		item_list = random_sample(rand, item_list, sample_n)
+
+		-- apply chances / randomized amounts and collect resulting items
+		local items = {}
+		for _, loot in ipairs(item_list) do
+			if rand:next(0, 1000) / 1000 <= loot.chance then
+				local itemdef = minetest.registered_items[loot.name]
+				local amount = 1
+				if loot.count ~= nil then
+					amount = rand:next(loot.count[1], loot.count[2])
+				end
+
+				if not itemdef then
+					minetest.log("warning", "Registered loot item " .. loot.name .. " does not exist")
+				elseif itemdef.tool_capabilities then
+					for n = 1, amount do
+						local wear = rand:next(0.20 * 65535, 0.75 * 65535) -- 20% to 75% wear
+						table.insert(items, ItemStack({name = loot.name, wear = wear}))
+					end
+				elseif itemdef.stack_max == 1 then
+					-- not stackable, add separately
+					for n = 1, amount do
+						table.insert(items, loot.name)
+					end
+				else
+					table.insert(items, ItemStack({name = loot.name, count = amount}))
+				end
+			end
+		end
+
+		-- place items at random places in chest
+		local inv = minetest.get_meta(pos):get_inventory()
+		local listsz = inv:get_size("main")
+		assert(listsz >= #items)
+		for _, item in ipairs(items) do
+			local index = rand:next(1, listsz)
+			if inv:get_stack("main", index):is_empty() then
+				inv:set_stack("main", index, item)
+			else
+				inv:add_item("main", item) -- space occupied, just put it anywhere
+			end
+		end
+	end
+	
+	function add_loot_adv(node, chest, dungeontype)
+		minetest.register_abm({
+			nodenames = {node},
+			interval = 1,
+			chance = 1,
+			action = function(pos)
+				if loot then
+					local facedir = minetest.get_node(pos).param2
+					minetest.remove_node(pos)
+					minetest.add_node(pos, {name = chest, param2 = facedir})
+					local noise = minetest.get_perlin(10115, 4, 0.5, 1)
+					populate_chest(pos, PcgRandom(noise3d_integer(noise, pos)), dungeontype)
+				end
+		end,
+		})
+	end
+	
+	function add_loot(name, description, chest, dungeontype)
+		minetest.register_node(minetest.get_current_modname() .. ":" .. name,
+			{ description = description,
+			tiles = {"default_chest_top.png", "default_chest_top.png", "default_chest_side.png", "default_chest_side.png", "default_chest_side.png", "default_chest_front.png"},
+			drawtype = "normal",
+			paramtype2 = "facedir",
+			groups = {choppy = 2, oddly_breakable_by_hand = 2},
+			sounds = default.node_sound_wood_defaults(),
+		})
+		minetest.register_abm({
+			nodenames = {minetest.get_current_modname() .. ":" .. name},
+			interval = 1,
+			chance = 1,
+			action = function(pos)
+				if loot then
+					local facedir = minetest.get_node(pos).param2
+					minetest.remove_node(pos)
+					minetest.add_node(pos, {name = chest, param2 = facedir})
+					local noise = minetest.get_perlin(10115, 4, 0.5, 1)
+					populate_chest(pos, PcgRandom(noise3d_integer(noise, pos)), dungeontype)
+				end
+		end,
+		})
+	end
+	
+	minetest.register_chatcommand("loot", {
+		params = "<enable/disable>",
+		description = "Disable or enable loot generation",
+		func = function(name, param)
+			if param == "disable" then
+				loot = false
+				minetest.chat_send_player(name, "Loot generation disabled")
+			elseif param == "enable" then
+				loot = true
+				minetest.chat_send_player(name, "Loot generation enabled")
+			end
+		end,
+	})
+
+	-- dungeon_loot.register({name = "default:dirt", chance = 0.6, count = {2, 16}, y = {-64, 32768}, types = {"normal", "sandstone", "desert"}})
+
+	minetest.register_node("xde_loot:loot_chest",
+		{ description = "Loot chest",
+		tiles = {"default_chest_top.png", "default_chest_top.png", "default_chest_side.png", "default_chest_side.png", "default_chest_side.png", "default_chest_front.png"},
+		drawtype = "normal",
+		paramtype2 = "facedir",
+		groups = {choppy = 2, oddly_breakable_by_hand = 2},
+		sounds = default.node_sound_wood_defaults(),
+	})
+	
+	add_loot_adv("xde_loot:loot_chest", "default:chest")
+	
+	if minetest.get_modpath("everness") then
+
+		minetest.register_node("xde_loot:everness_loot_chest",
+			{ description = "Everness loot chest",
+			tiles = {"everness_chest_top.png", "everness_chest_top.png", "everness_chest_side.png", "everness_chest_side.png", "everness_chest_side.png", "everness_chest_front.png"},
+			drawtype = "normal",
+			paramtype2 = "facedir",
+			groups = {choppy = 2, oddly_breakable_by_hand = 2},
+			sounds = default.node_sound_wood_defaults(),
+		})
+		
+		add_loot_adv("xde_loot:everness_loot_chest", "everness:chest")
+
+	end
+	
+end
